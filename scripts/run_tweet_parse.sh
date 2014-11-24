09448cf84d40550ab53bde86769906ea74925838
Wei-Liang <wl.chew.wl@gmail.com>
2014-11-24 14:27:57 +0800

    Added scripts folder and script for running parse.py

---
+++ scripts/run_tweet_parse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is intended to be run from the parent directory of its containing directory
+# i.e. $ ./scripts/run_tweet_parse.sh
+
+# The parent directory should contain the files 'parser.py' and 'train'
+# Output from parser.py will be stored in the file 'eval_trgdata.csv' in the parent directory
+
+python parser.py -i train -o eval_trgdata.csv
